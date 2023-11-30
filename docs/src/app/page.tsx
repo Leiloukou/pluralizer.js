dd81f1ce4e126133c6be1de52c7650f8e66a0aee
leiloukuou <jacobaleger12@gmail.com>
2023-11-30 18:10:53 -0500

    Leaving to do something else

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,5 +1,39 @@
-/** @format */
+import RepoTags from '@/components/RepoTags';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import Balancer from 'react-wrap-balancer';
 
 export default function Home() {
-	return <>Beluga</>;
+	return (
+		<div className='grid h-screen place-items-center'>
+			<Card className='max-w-[24rem]'>
+				<CardHeader className='space-y-3'>
+					{/* <div> */}
+					<div className='flex gap-2 items-center'>
+						<div className='w-5 bg-primary aspect-square rounded-full' />{' '}
+						<CardTitle>Pluralizer.js</CardTitle>
+					</div>
+					<CardDescription>
+						<Balancer>
+							Pluralizer.js is a simple type-safe lightweight
+							zero-dependency Javascript and Typescript library
+							for pluralizing strings.
+						</Balancer>
+					</CardDescription>
+					{/* </div> */}
+					<RepoTags />
+				</CardHeader>
+				<CardContent>
+					<p className='max-w-prose text-card-foreground'>
+						<Balancer>My cat's name is Beluga.</Balancer>
+					</p>
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
---
+++ docs/src/components/RepoTags.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import octokit from '@/lib/octokit';
+import { cn } from '@/lib/utils';
+import { useQuery } from '@tanstack/react-query';
+import { FC, PropsWithChildren, useEffect } from 'react';
+
+interface RepoTagsProps {}
+interface TopicProps extends PropsWithChildren {
+	className?: string;
+}
+
+const fetchTags = async () => {
+	// const response = await fetch(
+	// 		'https://api.github.com/repos/leiloukou/pluralizer.js'
+	// 	),
+	// 	data = (await response.json()) as unknown;
+
+	// return data;
+
+	const response = await octokit.rest.repos.getAllTopics({
+		owner: 'leiloukou',
+		repo: 'pluralizer.js',
+	});
+
+	if (response.status !== 200)
+		throw new Error(
+			'Unexpected error while fetching topics from Github repository.'
+		);
+
+	return response.data.names;
+};
+
+const Topic: FC<TopicProps> = ({ className, children }) => {
+	return (
+		<div
+			className={cn(
+				'py-1 px-3 bg-muted text-muted-foreground text-xs w-max rounded-full whitespace-nowrap',
+				className
+			)}>
+			{children}
+		</div>
+	);
+};
+
+const RepoTags: FC<RepoTagsProps> = ({}) => {
+	const { data: topics, isLoading } = useQuery({
+		queryKey: ['repo', 'tags'],
+		queryFn: fetchTags,
+	});
+
+	if (!isLoading && (!topics || !topics.length)) return null;
+
+	topics?.sort((a, b) => b.length - a.length);
+
+	return (
+		<div className='flex gap-2 overflow-x-auto'>
+			<div className='sr-only'>Topics: </div>
+			{!!topics ? (
+				topics.map(topic => <Topic key={topic}>{topic}</Topic>)
+			) : (
+				<>
+					<Topic className='animate-pulse'>Loading topics...</Topic>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default RepoTags;
